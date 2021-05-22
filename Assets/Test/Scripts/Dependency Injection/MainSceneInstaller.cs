4caade48ddec7973d83884188e100529458a804a
Ertan Turan <ertanturan@outlook.com>
2021-05-22 14:51:47 +0300

    Zenject and objectpooler validated in main scene

---
+++ Assets/Test/Scripts/Dependency Injection/MainSceneInstaller.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using CustomTools.ObjectPooling.Scripts.ObjectPool;
+using UnityEngine;
+using Zenject;
+
+public class MainSceneInstaller : MonoInstaller
+{
+
+    public override void InstallBindings()
+    {
+        Container.Bind<ObjectPooler>().FromComponentInChildren().AsSingle().NonLazy();
+    }
+}
