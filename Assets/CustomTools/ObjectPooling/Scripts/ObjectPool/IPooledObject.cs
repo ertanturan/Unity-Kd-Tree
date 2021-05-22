5c64d24908a67c4dcf530c424d53804797dbdc78
Ertan Turan <ertanturan@outlook.com>
2021-05-22 14:43:47 +0300

    Object Pooler 2.1.0 Imported (My own package)

---
+++ Assets/CustomTools/ObjectPooling/Scripts/ObjectPool/IPooledObject.cs
@@ -0,0 +1,16 @@
+﻿using CustomTools.ObjectPooling.Scripts.ObjectPool;
+using UnityEngine;
+using Zenject;
+
+public interface IPooledObject
+{
+    PooledObjectType PoolType { get; set; }
+
+     ObjectPooler Pooler { get; }
+     
+     [Inject]
+    void Construct(ObjectPooler pooler);
+    void OnObjectSpawn();
+    void OnObjectDespawn();
+    void Despawn();
+}
